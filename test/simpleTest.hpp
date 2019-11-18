8c8b6335df2fc1f2d9ecd19e2988c171908c9038
Anna Bernas <bernas.anna@icloud.com>
2019-11-18 21:11:32 +0100

    Add the Cmake and not working test

---
+++ test/simpleTest.hpp
@@ -0,0 +1,12 @@
+#include <gmock/gmock.h>
+
+using testing:::Eq;
+
+class SimpleTests : public testing::Test
+{};
+
+TEST_F(SimpleTests, add_two_to_two_equals_four)
+{
+    ASSERT_THAT(2+2, Eq(4));
+    sdasdas
+}
